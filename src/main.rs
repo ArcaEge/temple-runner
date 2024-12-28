984b73f36e1b7d07f64eba16c8cee8fda45879bf
Arca Cengiz <arcaegecengiz@gmail.com>
2024-12-28 15:24:43 +0000

    Added DefaultPlugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don't need to change any other names.
+        }
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, add_people)
+    .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+    .run();
 }
